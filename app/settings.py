e2a429f6d659d876363fe1f78a0f2c3ae23c85e1
Karan V <karanvknarayanan@gmail.com>
2020-09-04 17:13:00 +0530

    done right

--- app/settings.py
+++ app/settings.py
@@ -185,6 +185,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 AUTH_USER_MODEL = "core.User"
